7fd00214c58d4a8ff6252e546a0b3ebb40cd8047
viperit <313509188@qq.com>
2018-03-19 08:25:35 +0800

    preprocess.py update

--- preprocess.py
+++ preprocess.py
@@ -31,3 +31,211 @@ def read_and_split_train():
             writer.writerow(row)
 
 def creat_preprocessed_train_and_test():
+    i = 0
+    with open("./data/train_hard.csv", 'w', newline='') as csv_file:
+        writer = csv.writer(csv_file)
+        
+        csv_reader = csv.reader(open('./data/temp/preprocess/train_s_20.csv', encoding='utf-8'))
+        for row in csv_reader:
+            i+=1
+            if i==1:
+                row.append('hour')
+                row.append('isHoliday')
+                row.append('start_to_end_man_dis')
+                row.append('user_max_man_dis')
+                row.append('user_min_man_dis')
+                row.append('user_mean_man_dis')
+                row.append('user_mode_man_dis')
+                
+                row.append('user_cur_hour_used_ratio')
+                row.append('user_next_order_time_diff')
+                row.append('user_avg_dis_overall_avg_dis_ratio')
+                row.append('overall_avg_dis_cur_hour')
+                writer.writerow(row)
+                continue
+            user = row[1]
+            start = row[5]
+            end = row[6]
+            time = row[4]
+            
+            hour = time[time.find(' ')+1:time.find(' ')+3]
+            day = time[8:10]
+            
+            ### 当前小时和节假日信息
+            row.append(hour)
+            if day == '13' or day == '14' or day == '20' or day == '21':
+                row.append(1)
+            else:
+                row.append(0)
+            ###
+            
+            ### 加入曼哈顿距离
+            row.append(function.getManhattan(start,end))
+            ###
+            
+            ### 加入用户骑行信息
+            if user in user_dis_info:
+                row.append(user_dis_info[user][0])
+                row.append(user_dis_info[user][1])
+                row.append(user_dis_info[user][2])
+                if user_dis_info[user][3] != -1:
+                    row.append(user_dis_info[user][3])
+                else:
+                    row.append('')
+            else:
+                row.append('')
+                row.append('')
+                row.append('')
+                row.append('')
+            
+            ### 用户当前小时出发比例
+            if user in user_cur_hour_used_ratio:
+                if hour in user_cur_hour_used_ratio[user]:
+                    row.append(user_cur_hour_used_ratio[user][hour])
+                else:### 如果没有当前小时的记录
+                    row.append('')
+            else:
+                row.append('')
+            ###
+                
+            ### 计算 用户当前订单时间与下次订单时间的差值
+            cur_day = int(time[8:10])
+            cur_hour = int(hour)
+            if user in user_order_info_time:
+                index = 0
+                for order_info in user_order_info_time[user]:
+                    if order_info[4] == time and index < len(user_order_info_time[user]) - 1:
+                        nextTime = user_order_info_time[user][index+1][4]
+                        next_day = int(nextTime[8:10])
+                        next_hour = int(nextTime[nextTime.find(' ')+1:nextTime.find(' ')+3])
+                        
+                        totalHourDiff = (next_day-cur_day)*24+(next_hour-cur_hour)
+                        row.append(totalHourDiff)
+                        break
+                    if index == len(user_order_info_time[user]) - 1:
+                        row.append('')
+                        break
+                    index+=1
+            else:
+                row.append('')
+            ###
+            
+            ### 当前用户平均骑行距离／全部用户的骑行距离
+            if user in user_avg_dis_overall_avg_dis_ratio:
+                row.append(user_avg_dis_overall_avg_dis_ratio[user])
+            else:
+                row.append('')
+            ###
+            
+            ### 当前小时全用户的平均骑行距离
+            if hour in overall_avg_dis_cur_hour:
+                row.append(overall_avg_dis_cur_hour[hour])
+            else:
+                row.append('')
+            ###
+            writer.writerow(row)
+
+
+    i = 0
+    with open("./data/test_hard.csv", 'w', newline='') as csv_file:
+        writer = csv.writer(csv_file)
+        
+        csv_reader = csv.reader(open('./data/temp/preprocess/test_b_20.csv', encoding='utf-8'))
+        for row in csv_reader:
+            i+=1
+            if i==1:
+                row.append('hour')
+                row.append('isHoliday')
+                row.append('start_to_end_man_dis')
+                row.append('user_max_man_dis')
+                row.append('user_min_man_dis')
+                row.append('user_mean_man_dis')
+                row.append('user_mode_man_dis')
+                
+                row.append('user_cur_hour_used_ratio')
+                row.append('user_next_order_time_diff')
+                row.append('user_avg_dis_overall_avg_dis_ratio')
+                row.append('overall_avg_dis_cur_hour')
+                writer.writerow(row)
+                continue
+            user = row[1]
+            start = row[5]
+            end = row[6]
+            time = row[4]
+            
+            hour = time[time.find(' ')+1:time.find(' ')+3]
+            day = time[8:10]
+            
+            ### 当前小时和节假日信息
+            row.append(hour)
+            if day == '13' or day == '14' or day == '20' or day == '21':
+                row.append(1)
+            else:
+                row.append(0)
+            ###
+            
+            ### 加入曼哈顿距离
+            row.append(function.getManhattan(start,end))
+            ###
+            
+            ### 加入用户骑行信息
+            if user in user_dis_info:
+                row.append(user_dis_info[user][0])
+                row.append(user_dis_info[user][1])
+                row.append(user_dis_info[user][2])
+                if user_dis_info[user][3] != -1:
+                    row.append(user_dis_info[user][3])
+                else:
+                    row.append('')
+            else:
+                row.append('')
+                row.append('')
+                row.append('')
+                row.append('')
+            
+            ### 用户当前小时出发比例
+            if user in user_cur_hour_used_ratio:
+                if hour in user_cur_hour_used_ratio[user]:
+                    row.append(user_cur_hour_used_ratio[user][hour])
+                else:### 如果没有当前小时的记录
+                    row.append('')
+            else:
+                row.append('')
+            ###
+                
+            ### 计算 用户当前订单时间与下次订单时间的差值
+            cur_day = int(time[8:10])
+            cur_hour = int(hour)
+            if user in user_order_info_time:
+                index = 0
+                for order_info in user_order_info_time[user]:
+                    if order_info[4] == time and index < len(user_order_info_time[user]) - 1:
+                        nextTime = user_order_info_time[user][index+1][4]
+                        next_day = int(nextTime[8:10])
+                        next_hour = int(nextTime[nextTime.find(' ')+1:nextTime.find(' ')+3])
+                        
+                        totalHourDiff = (next_day-cur_day)*24+(next_hour-cur_hour)
+                        row.append(totalHourDiff)
+                        break
+                    if index == len(user_order_info_time[user]) - 1:
+                        row.append('')
+                        break
+                    index+=1
+            else:
+                row.append('')
+            ###
+            
+            ### 当前用户平均骑行距离／全部用户的骑行距离
+            if user in user_avg_dis_overall_avg_dis_ratio:
+                row.append(user_avg_dis_overall_avg_dis_ratio[user])
+            else:
+                row.append('')
+            ###
+            
+            ### 当前小时全用户的平均骑行距离
+            if hour in overall_avg_dis_cur_hour:
+                row.append(overall_avg_dis_cur_hour[hour])
+            else:
+                row.append('')
+            ###
+            writer.writerow(row)
